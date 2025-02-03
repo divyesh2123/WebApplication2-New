a54b91cf35f3fcf83f6dfb3eac70f8434214847d
lenovo <lenovo@LAPTOP-BLNTEBH7>
2025-02-03 09:55:59 +0530

    changes for crud operation

--- Controllers/VehicleInfoController.cs
+++ Controllers/VehicleInfoController.cs
@@ -6,9 +6,32 @@ namespace WebApplication2.Controllers
 {
     public class VehicleInfoController : Controller
     {
-        public IActionResult Index()
+
+        public ActionResult ListInfo()
+        {
+           using VmsContext vmsContext = new ();   
+            List<VehicleMaster> d = new List<VehicleMaster>();
+            d = vmsContext.VehicleMasters.ToList(); 
+            return View(d);
+        }
+
+
+        public IActionResult Index(int? id)
         {
             VehilceDatacs vehicleMaster = new VehilceDatacs();
+
+            if(id.HasValue)
+            {
+                VmsContext vmsContext = new VmsContext();
+                var p = vmsContext.VehicleMasters.Find(id);
+                vehicleMaster.PlateNumber = p.PlateNumber;
+                vehicleMaster.PassingYear= p.PassingYear;   
+                vehicleMaster.Make = p.Make;
+                vehicleMaster.IsActive = p.IsActive.Value;
+                vehicleMaster.Type = p.Type;    
+
+
+            }
 
             vehicleMaster.Items = new List<SelectListItem>();
 
@@ -28,21 +51,50 @@ namespace WebApplication2.Controllers
         [HttpPost]
         public IActionResult Index(VehicleMaster av)
         {
+
+            
             using VmsContext vmsContext = new();
 
-            VehicleMaster d = new VehicleMaster();
-            d.IsActive = av.IsActive;
-            d.PlateNumber = av.PlateNumber;
-            d.Model = av.Model;
-            d.Make = av.Make;
-            d.Type = av.Type;
-            d.PassingYear = av.PassingYear; 
+            if (av.Id == 0)
+            {
+                VehicleMaster d = new VehicleMaster();
+                d.IsActive = av.IsActive;
+                d.PlateNumber = av.PlateNumber;
+                d.Model = av.Model;
+                d.Make = av.Make;
+                d.Type = av.Type;
+                d.PassingYear = av.PassingYear;
+                vmsContext.VehicleMasters.Add(d);
+            }
+            else
+            {
+                var vehicle = vmsContext.VehicleMasters.Find(av.Id);
+                vehicle.PlateNumber = av.PlateNumber;
+                vehicle.Model = av.Model;
+                vehicle.Make = av.Make; 
+                vehicle.Type = av.Type;
+                vehicle.IsActive = av.IsActive; 
 
-            vmsContext.VehicleMasters.Add(d);
+            }
 
             vmsContext.SaveChanges();
 
-            return RedirectToAction("Index");
+            return RedirectToAction("ListInfo");
         }
+
+        public ActionResult DeleteInfo(int id)
+        {
+            using VmsContext vmsContext = new();
+
+            var d = vmsContext.VehicleMasters.Find(id);
+
+            vmsContext.VehicleMasters.Remove(d);
+
+            vmsContext.SaveChanges();
+
+            return RedirectToAction("ListInfo");   
+        }
+
+       
     }
 }
